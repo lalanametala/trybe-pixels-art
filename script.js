6c55f2f7f055bdcef7bba31501b40080396ca778
Lalá Nametala <lalanametala@gmail.com>
2021-12-09 15:29:36 -0300

    7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 window.onload = function () {
-    let palletFirstColor = document.querySelector('.color');
-    palletFirstColor.classList.add('selected');
+    let paletteFirstColor = document.querySelector('.color');
+    paletteFirstColor.classList.add('selected');
 }
+
+const blackButton = document.getElementById('black');
+const greenButton = document.getElementById('green');
+const pinkButton = document.getElementById('pink');
+const greyButton = document.getElementById('grey');
+
+
+function changeClass (event) {
+    const currentSelectedElement = document.querySelector('.selected');
+    if (currentSelectedElement.id != event.target.id){
+        currentSelectedElement.classList.remove('selected');
+        event.target.classList.add('selected');
+    }
+}
+
+blackButton.addEventListener('click', changeClass);
+greenButton.addEventListener('click', changeClass);
+pinkButton.addEventListener('click', changeClass);
+greyButton.addEventListener('click', changeClass);
