34af09cc967a7e3942ac85ff6e00cf16c4cd6350
Lalá Nametala <lalanametala@gmail.com>
2021-12-09 15:57:37 -0300

    8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ blackButton.addEventListener('click', changeClass);
 greenButton.addEventListener('click', changeClass);
 pinkButton.addEventListener('click', changeClass);
 greyButton.addEventListener('click', changeClass);
+
+blackButton.style.backgroundColor = 'black';
+greenButton.style.backgroundColor = 'green';
+pinkButton.style.backgroundColor = 'pink';
+greyButton.style.backgroundColor = 'grey';
+
+let firstLine = document.querySelector('#first-line');
+let secondLine = document.querySelector('#second-line');
+let thirdLine = document.querySelector('#third-line');
+let fourthLine = document.querySelector('#fourth-line');
+let fifthLine = document.querySelector('#fifth-line');
+
+function colorePixel (event){
+    let corSelecionada = document.querySelector('.selected');    
+    event.target.style.backgroundColor = corSelecionada.style.backgroundColor;
+}
+
+firstLine.addEventListener('click', colorePixel);
+secondLine.addEventListener('click', colorePixel);
+thirdLine.addEventListener('click', colorePixel);
+fourthLine.addEventListener('click', colorePixel);
+fifthLine.addEventListener('click', colorePixel);
